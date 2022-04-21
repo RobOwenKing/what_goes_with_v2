a16f9521b1d089e8dbb9408a70b9da23c494a37b
Robert King <robert.o.king@hotmail.co.uk>
2022-04-21 23:39:58 +0100

    test: Add failing/pending model specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'name' do
+    it { should validate_presence_of(:name) }
+  end
+  describe 'role' do
+    it 'should default to member'
+    it 'should ignore values as a param'
+  end
 end
