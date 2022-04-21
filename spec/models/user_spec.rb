476b623cfc5c638eb23152ca1924cb7b7b788b73
Robert King <robert.o.king@hotmail.co.uk>
2022-04-21 23:45:41 +0100

    test: Expand pending model specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,24 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
   end
   describe 'role' do
-    it 'should default to member'
-    it 'should ignore values as a param'
+    it 'should default to member' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password123',
+        password_confirmation: 'password123',
+        name: 'Dentarthurdent'
+      )
+      expect(user.role).to eq(1)
+    end
+    it 'should ignore values as a param' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password123',
+        password_confirmation: 'password123',
+        name: 'Dentarthurdent',
+        role: 2
+      )
+      expect(user.role).to eq(1)
+    end
   end
 end
