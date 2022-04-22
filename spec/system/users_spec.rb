62feef8d227d4fd71594e03fd6ddd9a639523bc2
Robert King <robert.o.king@hotmail.co.uk>
2022-04-22 23:43:12 +0100

    test: System test for successful User registration

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "User account management", type: :system do
+RSpec.describe 'User account management', type: :system do
   before do
     driven_by(:rack_test)
   end
@@ -12,12 +12,12 @@ RSpec.describe "User account management", type: :system do
 
     scenario 'creates account with valid params' do
       fill_in 'Email', with: 'fake@test.com'
-      fill_in 'Password', with: 'fakepassword'
       fill_in 'Confirm password', with: 'fakepassword'
+      fill_in 'Password', with: 'fakepassword'
       fill_in 'Display name', with: 'Display me'
       click_on 'Sign up'
 
-      expect(page).to have_content('You have successfully signed up.')
+      expect(page).to have_content('You have signed up successfully.')
     end
   end
 end
