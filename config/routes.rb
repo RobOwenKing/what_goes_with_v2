9f638d82779b680a71d184a5ffebb4e95eacb290
Robert King <robert.o.king@hotmail.co.uk>
2022-04-28 23:44:40 +0100

    feat: Ingredients resources routes, except index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'ingredients/show'
-  get 'ingredients/new'
-  get 'ingredients/create'
-  get 'ingredients/edit'
-  get 'ingredients/update'
-  get 'ingredients/delete'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :ingredients, except: %i[index show]
+
   root 'pages#home'
 end
