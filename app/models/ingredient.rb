ca0cc35e4d704b890b09ebce309c28af0dad52af
Robert King <robert.o.king@hotmail.co.uk>
2022-05-02 20:23:35 +0100

    feat: Custom to_param in Ingredient Model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,12 @@ class Ingredient < ApplicationRecord
   validates :name, :slug, presence: true
   validates :slug, uniqueness: true
 
+  def to_param
+    return nil unless persisted?
+
+    slug
+  end
+
   def self.find(input)
     input.to_i == input ? super : find_by_slug(input)
   end
