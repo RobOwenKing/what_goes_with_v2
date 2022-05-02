9b23af89fcc5e947bdc205ee5893661f9acbb4b4
Robert King <robert.o.king@hotmail.co.uk>
2022-05-02 12:01:06 +0100

    feat: Pretty URL for Ingredients using slug with custom self.find in Model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   validates :name, :slug, presence: true
   validates :slug, uniqueness: true
+
+  def self.find(input)
+    input.to_i == input ? super : find_by_slug(input)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :ingredients, except: %i[index show]
+  resources :ingredients, except: %i[index]
 
   root 'pages#home'
 end
