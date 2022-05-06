d4c4861730bcaeeb5364aae0ab5be4acdadba33b
Robert King <robert.o.king@hotmail.co.uk>
2022-05-06 22:27:46 +0100

    feat: Template searchInputHandler() calling Rails.ajax()

--- app/javascript/source/ajaxSearch.js
+++ app/javascript/source/ajaxSearch.js
@@ -1,6 +1,20 @@
+import Rails from "@rails/ujs";
+
+const searchInputHandler = (event) => {
+  console.log(event.target.value);
+
+  Rails.ajax({
+    type: 'GET',
+    url: '/ingredients',
+    data: `q=${event.target.value}`,
+    success: () => { console.log('Success!') },
+    failure: () => { console.log('Fail!') }
+  });
+};
+
 export const initSearch = () => {
   document.querySelectorAll('[data-search="ingredients"]')
-          .forEach(ele => {
-            console.log(ele);
-          })
+      .forEach(ele => {
+        ele.addEventListener('input', searchInputHandler);
+      })
 };
