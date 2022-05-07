1f52b1ec2dd0f77b9b5b11459f419b0aecbe838c
Robert King <robert.o.king@hotmail.co.uk>
2022-05-07 22:08:23 +0100

    docs: Add comments in source/ajaxSearch.js

--- app/javascript/source/ajaxSearch.js
+++ app/javascript/source/ajaxSearch.js
@@ -1,10 +1,17 @@
 import Rails from "@rails/ujs";
 
+/**
+  * Update autocomplete suggestions of ingredient links with given data
+  * @param {JSON} data - Data from the server
+*/
 const searchResultsHandler = (data) => {
   console.log(data);
 };
 
-
+/**
+  * Submits search via AJAX and calls function to update page if request successful
+  * @param {InputEvent} event
+*/
 const searchInputHandler = (event) => {
   if (event.target.value.length < 1) { return; }
 
@@ -17,9 +24,12 @@ const searchInputHandler = (event) => {
   });
 };
 
+/**
+  * Attach input event listeners to all search boxes on the page for live updates
+*/
 export const initSearch = () => {
   document.querySelectorAll('[data-search="ingredients"]')
-      .forEach(ele => {
-        ele.addEventListener('input', searchInputHandler);
-      })
+          .forEach(ele => {
+            ele.addEventListener('input', searchInputHandler);
+          });
 };
