c40ef999ebeb168c0a9618731f112a648a5a542a
Robert King <robert.o.king@hotmail.co.uk>
2022-05-16 01:22:59 +0100

    refactor: Stop validating Pair reference uniqueness scoped in both directions to skip unnecessary steps - just one should do

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -3,7 +3,8 @@ class Pair < ApplicationRecord
   belongs_to :ingredient2, class_name: 'Ingredient'
 
   validates :ingredient1, uniqueness: { scope: :ingredient2 }
-  validates :ingredient2, uniqueness: { scope: :ingredient1 }
+  # Validating in one direction should be enough!
+  # validates :ingredient2, uniqueness: { scope: :ingredient1 }
 
   validates :slug, presence: true
 end
