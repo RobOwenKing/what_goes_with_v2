560a8f7578deac074d1acd3519e15cc436f31f14
Robert King <robert.o.king@hotmail.co.uk>
2022-05-16 01:11:13 +0100

    feat: Add validation of Pair uniqueness (broken test!)

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -2,5 +2,8 @@ class Pair < ApplicationRecord
   belongs_to :ingredient1, class_name: 'Ingredient'
   belongs_to :ingredient2, class_name: 'Ingredient'
 
+  validates :ingredient1, uniqueness: { scope: :ingredient2 }
+  validates :ingredient2, uniqueness: { scope: :ingredient1 }
+
   validates :slug, presence: true
 end
