f67bf36296cd64d4cac8607aa2ab69c4c3dc155c
Robert King <robert.o.king@hotmail.co.uk>
2022-05-19 01:28:55 +0100

    test: Add basic checks of Pair model #alphabetise_order and #create_slug

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe Pair, type: :model do
 
     it { should validate_presence_of(:slug) }
   end
+
+  describe 'format' do
+    before do
+      @cheese = create(:ingredient)
+      @courgette = create(:ingredient, name: 'Courgette', slug: 'courgette', aka: 'Zucchini', eg: '')
+
+      # We need to use #create instead of #new because alphabetising of
+      # ingredients happens before_validation
+      @pair1 = Pair.create(ingredient1: @cheese, ingredient2: @courgette)
+      @pair2 = Pair.create(ingredient1: @courgette, ingredient2: @cheese)
+    end
+
+    it 'should ensure Ingredients are in alphabetical order' do
+      expect(@pair1.ingredient1.slug < @pair1.ingredient2.slug).to be_truthy
+      expect(@pair2.ingredient1.slug < @pair2.ingredient2.slug).to be_truthy
+    end
+
+    it 'should generate a slug in the correct format' do
+      expect(@pair1.slug).to eq('courgette-hard-cheese')
+      expect(@pair2.slug).to eq('courgette-hard-cheese')
+    end
+  end
 end
