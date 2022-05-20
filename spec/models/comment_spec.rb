2cbcdf5f04d559a31ce1cb2a8c1536a83b73dba5
Robert King <robert.o.king@hotmail.co.uk>
2022-05-20 17:35:47 +0100

    test: Add Comment model association and validation specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'associations' do
+    it { should belong_to(:commentable) }
     it { should belong_to(:user) }
   end
 
