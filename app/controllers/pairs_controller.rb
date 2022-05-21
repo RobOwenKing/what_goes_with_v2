93264035e523afb41554af528e3ee019d4afe8fa
Robert King <robert.o.king@hotmail.co.uk>
2022-05-22 00:33:25 +0100

    feat: Introduce Pair _pair view partial so show and new will be kept dry

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -4,7 +4,9 @@ class PairsController < ApplicationController
   end
 
   def show
-    @pair ||= Pair.find(params[:id])
+    @pair = Pair.find(params[:id])
+    @ingredient1 = @pair.ingredient1
+    @ingredient2 = @pair.ingredient2
   end
 
   def new
@@ -14,7 +16,10 @@ class PairsController < ApplicationController
 
   def redirect_to_show_or_new
     @ingredients = [params[:ing1], params[:ing2]].sort
+
     @pair = Pair.find("#{@ingredients[0]}-#{@ingredients[1]}")
+    @ingredient1 = Ingredient.find(@ingredients[0])
+    @ingredient2 = Ingredient.find(@ingredients[1])
 
     render 'show' and return if @pair
 
