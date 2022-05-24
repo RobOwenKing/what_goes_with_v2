2f0270670035a0759a0a24daa5185c03bf067f37
Robert King <robert.o.king@hotmail.co.uk>
2022-05-25 00:09:25 +0100

    feat: Add Comment create action (and necessary private methods)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :set_commentable, only: :create
+
   def new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.commentable = @commentable
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:notice] = 'Comment posted successfully.'
+    else
+      flash[:alert] = 'Sorry, there was an issue with saving that comment.'
+    end
+
+    redirect_back(fallback_location: root_path)
   end
 
   def edit
@@ -12,5 +25,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_commentable
+    if params[:comment][:comment]
+      @commentable = Comment.find(params[:comment][:comment])
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
